b1d94e1c71292e2670773546ca3b78d7bd0f8635
nachofernan <nachofernan@gmail.com>
2025-09-16 10:44:25 -0300

    v

--- resources/views/proveedores/proveedors/show.blade.php
+++ resources/views/proveedores/proveedors/show.blade.php
@@ -306,8 +306,11 @@
                                         </div>
                                 </div>
                                 <div class="flex items-center space-x-2">
+                                    <svg class="h-4 w-4 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                                    </svg>
                                     <a href="{{ route('proveedores.documentos.show', $apoderado->last_documento) }}" 
-                                        class="text-blue-600 hover:underline" target="_blank">
+                                        class="text-blue-600 hover:underline text-xs mr-2" target="_blank">
                                         Descargar
                                     </a>
                                     @can('Proveedores/Proveedores/Editar')
